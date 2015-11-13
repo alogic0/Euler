381cb7e6eb4e0084e975eb1c60ff84ed86188098
Oleg Tsybulskyi <alogic0@gmail.com>
2015-11-13 04:10:48 +0200

    Up to 45 problem and investigation

---
+++ euler45.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Euler
+
+--euler45 40755 -> 1533776805
+--euler45 1533776805 -> ?
+-- put current number here and recompile 
+-- ghc -O2 euler45.hs
+
+main = euler45 20261994464935
